6da5c7b99938639b3495020661a8df2a3452a66a
mohamed <mbouhdid@gmail.com>
2024-01-17 07:12:24 +0100

    finalisation tuto nextjs

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,22 +9,22 @@ import { RevenueChartSkeleton, InvoiceSkeleton, LatestInvoicesSkeleton, CardsSke
 export default async function Page() {
     return (
         <main>
-        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-            Dashboard
-        </h1>
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            <Suspense fallback={<CardsSkeleton/>}>
-                <CardWrapper></CardWrapper>
-            </Suspense>
-        </div>
-        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-            <Suspense fallback={<RevenueChartSkeleton />}>
-                <RevenueChart />
-            </Suspense>
-            <Suspense fallback={<LatestInvoicesSkeleton/>}>
-                <LatestInvoices/>
-            </Suspense>
-        </div>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton/>}>
+                    <CardWrapper></CardWrapper>
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices/>
+                </Suspense>
+            </div>
         </main>
     );
 }
